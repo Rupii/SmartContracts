3e84647cd63bd857f80114edecbb0979613cb3dd
rupii <mudavathrupesh@gmail.com>
2018-03-21 22:55:05 +0530

    mocha pre test

--- messanger/test/inbox.test.js
+++ messanger/test/inbox.test.js
@@ -3,28 +3,4 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-class Car {
-    park() {
-        return 'stopped';
-    }
 
-    drive() {
-        return 'vroommm';
-    }
-}
-
-let car;
-
-beforeEach(() =>{
-    car = new Car();
-});
-describe('Car', () =>{
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroommm');
-    });
-    
-});
